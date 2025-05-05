33bb3892bbd0dea28e0439309658c98cdadc5a27
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-05-05 08:23:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/super_heroes_app/ui/theme/Type.kt
@@ -0,0 +1,40 @@
+package com.example.super_heroes_app.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.super_heroes_app.R
+
+val cabinBold = FontFamily(
+    Font(R.font.cabin_bold)
+)
+val cabinRegular= FontFamily(
+    Font(R.font.cabin_regular)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = cabinRegular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily =cabinBold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = cabinBold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = cabinRegular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
+)
